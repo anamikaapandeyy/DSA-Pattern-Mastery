3fe1999f2b25aed4bfe4668b6464a824642a0ad7
anamikaapandeyy <anamikapandey0303@gmail.com>
2026-05-23 23:58:04 +0530

    Update LC-2824-Count-Pairs-Whose-Sum-is-less-than-Target.cpp
    Optimized pair counting using sorting and two pointers

--- LC-2824-Count-Pairs-Whose-Sum-is-less-than-Target.cpp
+++ LC-2824-Count-Pairs-Whose-Sum-is-less-than-Target.cpp
@@ -9,7 +9,7 @@ Complexity:
 - Time: O(N²)
 - Space: O(1)
 */
-
+/*
 class Solution {
 public:
     int countPairs(vector<int>& nums, int target) {
@@ -20,6 +20,43 @@ public:
                 if(nums[i] + nums[j] < target) {
                     count++;
                 }
+            }
+        }
+
+        return count;
+    }
+};
+*/
+
+
+
+/*
+Problem: Count Pairs Whose Sum is Less than Target
+Link: https://leetcode.com/problems/count-pairs-whose-sum-is-less-than-target/
+Description:
+Count the number of index pairs whose sum is less than the given target.
+Approach:
+Uses sorting with two pointers. If the current pair sum is less than target,all elements between the pointers form valid pairs with the left element,allowing multiple counts at once.
+Complexity:
+- Time: O(N log N)
+- Space: O(1)
+*/
+
+class Solution {
+public:
+    int countPairs(vector<int>& nums, int target) {
+        sort(nums.begin(), nums.end());
+
+        int left = 0, right = nums.size() - 1;
+        int count = 0;
+
+        while(left < right) {
+            if(nums[left] + nums[right] < target) {
+                count += (right - left);
+                left++;
+            }
+            else {
+                right--;
             }
         }
 
